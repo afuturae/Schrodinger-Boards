5165b90a4c5d1b5aa9e8acff40cbb34504ff91bf
victor <lbs.vvieira@ifood3rd.com>
2022-03-28 22:02:02 -0300

    first commit

---
+++ src/main/java/br/com/schrodinger/boards/schrodingerboards/SchrodingerBoardsApplication.java
@@ -0,0 +1,13 @@
+package br.com.schrodinger.boards.schrodingerboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchrodingerBoardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchrodingerBoardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/schrodinger/boards/schrodingerboards/SchrodingerBoardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.schrodinger.boards.schrodingerboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchrodingerBoardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
